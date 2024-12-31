35169efd16939e929019cb1dd28baac7caa8d463
crasowas <110958409+crasowas@users.noreply.github.com>
2024-12-31 18:49:18 +0800

    Improve performance of API usage analysis

--- fixer.sh
+++ fixer.sh
@@ -174,33 +174,35 @@ function get_dependency_name() {
 function analyze_binary_file() {
     local file_path="$1"
     local -a results=()
-    
+
+    # Check if the API symbol exists in the binary file using `nm` and `strings`
+    local nm_output=$(nm "$file_path" 2>/dev/null | xcrun swift-demangle)
+    local strings_output=$(strings "$file_path")
+    local combined_output="$nm_output"$'\n'"$strings_output"
+
     for api_symbol in "${API_SYMBOLS[@]}"; do
         local substrings=($(split_string_by_delimiter "$api_symbol"))
         local category=${substrings[0]}
         local api=${substrings[1]}
-    
-        # Check if the API symbol exists in the binary file using `nm` and `strings`
-        for tool in "nm \"$file_path\" 2>/dev/null | xcrun swift-demangle" "strings \"$file_path\""; do
-            if eval "$tool | grep -E \"$api\$\" >/dev/null"; then
-                local index=-1
-                for ((i=0; i < ${#results[@]}; i++)); do
-                    local result="${results[i]}"
-                    local result_substrings=($(split_string_by_delimiter "$result"))
-                    # If the category matches an existing result, update it
-                    if [ "$category" == "${result_substrings[0]}" ]; then
-                        index=i
-                        results[i]="${result_substrings[0]}$DELIMITER${result_substrings[1]},$api$DELIMITER${result_substrings[2]}"
-                        break
-                    fi
-                done
 
-                # If no matching category found, add a new result
-                if [[ $index -eq -1 ]]; then
-                    results+=("$category$DELIMITER$api$DELIMITER$(encode_path "$file_path")")
+        if echo "$combined_output" | grep -E "$api\$" >/dev/null; then
+            local index=-1
+            for ((i=0; i < ${#results[@]}; i++)); do
+                local result="${results[i]}"
+                local result_substrings=($(split_string_by_delimiter "$result"))
+                # If the category matches an existing result, update it
+                if [ "$category" == "${result_substrings[0]}" ]; then
+                    index=i
+                    results[i]="${result_substrings[0]}$DELIMITER${result_substrings[1]},$api$DELIMITER${result_substrings[2]}"
+                    break
                 fi
+            done
+
+            # If no matching category found, add a new result
+            if [[ $index -eq -1 ]]; then
+                results+=("$category$DELIMITER$api$DELIMITER$(encode_path "$file_path")")
             fi
-        done
+        fi
     done
     
     echo "${results[@]}"
@@ -480,12 +482,16 @@ function generate_report() {
     echo ""
 }
 
+start_time=$(date +%s)
+
 generate_report true
 fix_frameworks
 fix_app
 generate_report false
 
-echo "🎉 All fixed!"
+end_time=$(date +%s)
+
+echo "🎉 All fixed! ⏰ $((end_time - start_time)) seconds"
 echo "🌟 If you found this script helpful, please consider giving it a star on GitHub. Your support is appreciated. Thank you!"
 echo "🔗 Homepage: https://github.com/crasowas/app_privacy_manifest_fixer"
 echo "🐛 Report issues: https://github.com/crasowas/app_privacy_manifest_fixer/issues"
